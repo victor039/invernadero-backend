3782168dddff3b06f30cd473d30ae76e4e60f635
Victor Navarrete <jacksonvictor477@gmail.com>
2026-05-29 09:38:13 -0600

    Asegurar columnas de ventas al iniciar

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
+const { DataTypes } = require('sequelize');
 require('./src/models');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
+async function asegurarColumnasVentas() {
+    const queryInterface = sequelize.getQueryInterface();
+    const tablaVentas = await queryInterface.describeTable('ventas');
+    const columnasNecesarias = {
+        tipo_cliente: { type: DataTypes.STRING, defaultValue: 'registrado' },
+        cliente_paso: { type: DataTypes.STRING, allowNull: true },
+        metodo_pago: { type: DataTypes.STRING, allowNull: true },
+        referencia_pago: { type: DataTypes.STRING, allowNull: true },
+        ticket_descarga: { type: DataTypes.BOOLEAN, defaultValue: true },
+        ticket_correo: { type: DataTypes.BOOLEAN, defaultValue: false },
+        correo_ticket: { type: DataTypes.STRING, allowNull: true }
+    };
+
+    for (const [columna, definicion] of Object.entries(columnasNecesarias)) {
+        if (!tablaVentas[columna]) {
+            await queryInterface.addColumn('ventas', columna, definicion);
+        }
+    }
+
+    await sequelize.query("UPDATE ventas SET tipo_cliente = 'registrado' WHERE tipo_cliente IS NULL");
+    await sequelize.query('UPDATE ventas SET ticket_descarga = true WHERE ticket_descarga IS NULL');
+    await sequelize.query('UPDATE ventas SET ticket_correo = false WHERE ticket_correo IS NULL');
+}
+
 async function startServer() {
     try {
 
         await sequelize.authenticate();
+        await asegurarColumnasVentas();
 
         if (process.env.DB_SYNC === 'true') {
             await sequelize.sync({ alter: true });
